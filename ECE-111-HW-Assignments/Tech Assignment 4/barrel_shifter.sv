a793f3f99465b4ee492650f41784b5553fc6c073
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-06 11:15:37 -0800

    Update barrel_shifter.sv

--- ECE-111-HW-Assignments/Tech Assignment 4/barrel_shifter.sv
+++ ECE-111-HW-Assignments/Tech Assignment 4/barrel_shifter.sv
@@ -7,10 +7,14 @@ module barrel_shifter (
   output logic[3:0] dout
 );
 
+	
+// setting wires
   logic [3:0] inv_din;
   logic [3:0] inv_dout;
   logic mmux31, mmux21, mmux71, mux0mux4, mux1mux5, mux2mux6, mux3mux7;
 
+// first of three always blocks. This block just inverts the din coming into the barrel shifter if it is going left. 
+// If it is right then it won't invert
   always_comb begin
      if(direction == 1) begin
        inv_din[0] = din[3];
@@ -25,6 +29,11 @@ module barrel_shifter (
   end
 end
 
+	
+// second always block. If it is a left or right shift it sets the input 1 of Mux 2, Mux 3, and Mux 7 to 0. If it is rotate then
+// the value assigned to each Mux is inv_din[0], inv_din[1], and for 7 it is either inv_din[2] or inv_din[0]. 
+// for inv_din these are the values assigned to in the first always block when you inverse or don't
+	
 always_comb begin
 
 mmux21 = 0;
@@ -70,6 +79,8 @@ end
 end
 end
 
+	
+// third always block the flips inv_dout to dout
 always_comb begin
 if(direction == 1) begin
 	dout[0] = inv_dout[3];
@@ -84,6 +95,8 @@ end else begin
 end
 end
 
+	
+// mux connections
 mux_2x1 m0 (.in0(inv_din[0]),.in1(inv_din[2]),.sel(shift_value[1]),.out(mux0mux4));
 mux_2x1 m1 (.in0(inv_din[1]),.in1(inv_din[3]),.sel(shift_value[1]),.out(mux1mux5));
 mux_2x1 m2 (.in0(inv_din[2]),.in1(mmux21),.sel(shift_value[1]),.out(mux2mux6));
